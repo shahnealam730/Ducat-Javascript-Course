179e6c1bff34112c638eac688968df6d59d7b761
shahnealam730 <shahne.developer@gmail.com>
2025-05-29 12:54:35 +0530

    add loop

--- 5_loops.js
+++ 5_loops.js
@@ -66,16 +66,84 @@
 // console.log("Congratulations! You've guessed the secret number:", secretNumber);
 
 
-let guess = prompt("Guess a number");
-const secretNumber = 10;
+// let guess = prompt("Guess a number");
+// const secretNumber = 10;
 
-while (guess != secretNumber) {
-    if(guess < secretNumber) {
-        alert("Too low! Try again.");
-    }else {
-        alert("Too high! Try again.");
-    }
-    guess = prompt("Guess a number");
+// while (guess != secretNumber) {
+//     if(guess < secretNumber) {
+//         alert("Too low! Try again.");
+//     }else {
+//         alert("Too high! Try again.");
+//     }
+//     guess = prompt("Guess a number");
     
-}
-alert("Congratulations! You've guessed the secret number:", secretNumber);
+// }
+// alert("Congratulations! You've guessed the secret number:", secretNumber);
+
+
+// // Print the multiplication table of 5 from 1 to 10
+// let num = 5;
+// for(let i = 1; i <= 10; i++) {
+//     console.log(num + " x " + i + " = " + (num * i));
+// };
+
+
+
+// Print the multiplication table of 5 from 1 to 10
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + " is even");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
+
+
+// Print the squares of numbers from 1 to 15
+
+// for (let i = 1; i <= 15; i++) {
+//     console.log("Square of", i, "is", i * i);
+// };
+
+
+
+
+// calculate the sum of first 100 natural numbers
+
+
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+// console.log("Sum of first 100 natural numbers:", sum);
+
+
+
+// factorial of a number 5;
+// let num = 4;
+// let factorial = 1;
+// for (let i = 1; i <= num; i++) {
+//     factorial *= i; // factorial = factorial * i;
+// };
+// console.log("Factorial of", num, "is", factorial);
+
+
+// let num = 5;
+// let factorial = 1;
+// while (num > 0) {
+//     factorial *= num; // factorial = factorial * num;
+//     num--; // decrement num by 1
+// };
+// console.log("Factorial is", factorial); // Output: Factorial is 120
+
+
+
+// for(let i = 1; 1<=10; i++){
+//     if(i == 6){
+//         break
+//     }
+//     console.log("Loop ended at i =", i); // This will not execute because the loop condition is incorrect
+// };
+
+
+
