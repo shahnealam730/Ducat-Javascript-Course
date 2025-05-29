e267d001ed0f3652e1e3c74b972287a912bdcbda
shahnealam730 <shahne.developer@gmail.com>
2025-05-30 01:13:10 +0530

    Add more content

--- 5_loops.js
+++ 5_loops.js
@@ -1,149 +1,156 @@
-// // // // // while (condition) {
-// // // // //     // code to be executed repeatedly as long as condition is true
-// // // // //     // for example, incrementing a counter
-// // // // //     // let i = 0;
-// // // // //     // while (i < 5) {
-// // // // //     //     console.log(i);
-// // // // //     //     i++;
-// // // // //     // }
-    
-// // // // // }
+// LOOPS: a block of code will execute until certain condition met.
+// why we need loop?
+// Hello world 10th times
+// 1 - 10
+// 100 - 50
+// Basic js:-> While loop, do-while,for
+// WHILE LOOP: conditional based
+// syntax:
+// initization;
+// while(condition){ // if true
+// code to be executed;
+// increment/decrement
+// }
 
+// 1 - 10
+// let num = 1;
+// // while(num <= 10) {
+// while (num < 11) { // 10 < 11 , 2<11,3<11 , 11 < 11(false)
+//     console.log(num); // 1,2,3
+//     // num = num + 1; // 1 + 1 =2,2+1=3,3+1=4 , 10+1=11
+//     num++;
+// }
+// console.log("Loop End")
 
-// // // // let value = 10;
+// let value = 1;
+// while (value <= 10) {
+//     document.writeln("Hello World...");
+//     document.writeln("<br>")
+//     value++;
+// }
 
-// // // // while (value <= 100) {
-// // // //     console.log(value);
-// // // //     value += 10; // increment value by 10
-    
-// // // // }
+// Print the series: 10,20,30,40,...,100
+// let num = 10;
+// while (num <= 100) {
+//     console.log(num)
+//     // num = num + 10;
+//     num += 10;
+// }
+// Print the series: 105,98,91,....,7
+// let num = 105;
+// while (num >= 7) {
+//     console.log(num);
+//     num -= 7;
+// }
 
-
-// // // let value = 105;
-
-// // // while (value >= 7) {
-// // //     console.log(value);
-// // //     value -= 7; // increment value by 7
-    
-// // // }
-
-
-
-// // // let value = 4;
-// // // while (value <= 20) {
-// // //     if (value % 2 === 0) { // check if value is even
-// // //    console.log(value); // print value if it is even
-// // //     }
-   
-// // //     value++; // increment value by 4
-// // // }
-
-
-// // let sum = 0;
-// // let num = 10;
-// // while (num <= 50) {
-// //     if (num % 2 === 0) {
-// //         sum += num;
-// //     }
-// //     num++;
-// // }
-// // console.log("Sum of even numbers from 10 to 50:", sum);
-
-
-
-// let guess = prompt("Guess a number..?");
-// let secretNumber = 10;
-
-// while (parseInt(guess) !== secretNumber) {
-//     if (parseInt(guess) < secretNumber) {
-//         console.log("Too low! Try again.");
+// print even numbers 4 - 20
+// let even_num = 4;
+// while (even_num <= 20) { // 4 - 20
+//     if (even_num % 2 == 0) {
+//         console.log("Even Number: " + even_num);
 //     } else {
-//         console.log("Too high! Try again.");
+//         console.log("Odd Number: " + even_num)
 //     }
-//     guess = prompt("Guess a number..?");
-   
+//     even_num++;
 // }
-// console.log("Congratulations! You've guessed the secret number:", secretNumber);
-
-
-// let guess = prompt("Guess a number");
-// const secretNumber = 10;
-
-// while (guess != secretNumber) {
-//     if(guess < secretNumber) {
-//         alert("Too low! Try again.");
-//     }else {
-//         alert("Too high! Try again.");
-//     }
-//     guess = prompt("Guess a number");
-    
-// }
-// alert("Congratulations! You've guessed the secret number:", secretNumber);
-
-
-// // Print the multiplication table of 5 from 1 to 10
-// let num = 5;
-// for(let i = 1; i <= 10; i++) {
-//     console.log(num + " x " + i + " = " + (num * i));
-// };
-
-
-
-// Print the multiplication table of 5 from 1 to 10
-// for (let i = 0; i <= 15; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i + " is even");
-//     } else {
-//         console.log(i + " is odd");
-//     }
-// }
-
-
-// Print the squares of numbers from 1 to 15
-
-// for (let i = 1; i <= 15; i++) {
-//     console.log("Square of", i, "is", i * i);
-// };
-
-
-
-
-// calculate the sum of first 100 natural numbers
-
-
+// calculate the sum of even numbers 10 - 50
+// let num = 10;
 // let sum = 0;
-// for (let i = 1; i <= 100; i++) {
-//     sum += i;
+// while (num <= 50) { // 10 - 50
+//     if (num % 2 == 0) { // even numbers - 10,12,14,...50
+//         sum += num;
+//         // sum = sum + num;
+//     }
+//     num++;
 // }
-// console.log("Sum of first 100 natural numbers:", sum);
+// console.log("The sum of even numbers: " + sum);
 
 
+// let guess = prompt("Guess a number..?")
+// let secretNumber = 10;
+// // print you guessed it right
+// // try again
 
-// factorial of a number 5;
-// let num = 4;
-// let factorial = 1;
-// for (let i = 1; i <= num; i++) {
-//     factorial *= i; // factorial = factorial * i;
-// };
-// console.log("Factorial of", num, "is", factorial);
+// while (guess != secretNumber) { // 10 == 10
+//     guess = prompt("Try again!")
+// }
+// alert("Correct !")
 
+// FOR LOOP:
+// syntax:
+// for(initialization;condition;increment/decrement){
+// code to be executed}
+// 0 -10
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+// 0 2 4 6 8 10
+// for (let i = 0; i <= 10; i += 2) {
+//     console.log(i);
+// }
+// multiplication table of 5 using for loop
 
 // let num = 5;
-// let factorial = 1;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(num + "x" + i + "=" + num * i)
+// }
+
+//  Write a JavaScript for loop that iterates from 0 to 15. For each iteration, it checks if the
+// current number is odd or even, and displays a message on the screen.
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"....
+
+// PRINT THE SQUARE OF NUMBERS FROM 1 TO 15.
+// CALCULATE THE SUM OF FIRST 100 NATURAL NUMBERS.
+// FACTORIAL OF 5.
+//  5! 5 * 4 * 3 * 2 * 1 - while,for
+// let num = 6;
+// let fact = 1;
+// for (let i = 1; i <= num; i++) {
+//     fact *= i;
+// }
+
+// let num = 5;
+// let fact = 1;
 // while (num > 0) {
-//     factorial *= num; // factorial = factorial * num;
-//     num--; // decrement num by 1
-// };
-// console.log("Factorial is", factorial); // Output: Factorial is 120
+//     fact *= num;
+//     num--;
+// }
+// console.log(fact)
 
+// BREAK: IMMEDIETLY EXIT THE LOOP WHEN A CONDITION IS MET.
+// STOP SEARCHING WHEN THE ITEM IS FOUND
 
-
-// for(let i = 1; 1<=10; i++){
-//     if(i == 6){
-//         break
+// 1 - 5
+// for (let i = 1; i <= 10; i++) { // 1 - 10
+//     if (i == 6) { // 6 == 6
+//         break; // jump to 134 line
 //     }
-//     console.log("Loop ended at i =", i); // This will not execute because the loop condition is incorrect
-// };
+//     console.log(i)
+// }
+
+//  CONTINUE: SKIT THE CURRENT ITERATION AND JUMP TO THE NEXT ONE.
+// SKIP THE UNWANTED VALUES(LIKE EVEN NUMBERS OR BLANKS)
+
+// 1 2 3 4 6  8 9 10
+// for (let i = 1; i <= 10; i++) {
+//     if (i == 5 || i == 7) { // 5 == 5
+//         continue;
+//     }
+//     console.log(i)
+// }
 
 
-
+// do-while loop: A do-while loop is similar to a while loop, except that it will always execute the block of code at least once, even if the condition is initially false.
+// let i = 10;
+// do {
+//     // code to be executed
+//     // increment/decrement
+//     console.log(i); // 10
+//     i++;
+// }
+// // condition
+// while (i < 10) // 9 < 10
--- test.js
+++ test.js
@@ -1,258 +1,337 @@
-// // // 1. Check which one is the smallest number (2 numbers)
-// // let a = 10;
-// // let b = 20;
+// // // // 1. Check which one is the smallest number (2 numbers)
+// // // let a = 10;
+// // // let b = 20;
 
-// // if (a < b) {
-// //     console.log(a + " is the smallest");
-// // } else {
-// //     if (b < a) {
-// //         console.log(b + " is the smallest");
-// //     } else {
-// //         console.log("Both numbers are equal");
-// //     }
-// // }
+// // // if (a < b) {
+// // //     console.log(a + " is the smallest");
+// // // } else {
+// // //     if (b < a) {
+// // //         console.log(b + " is the smallest");
+// // //     } else {
+// // //         console.log("Both numbers are equal");
+// // //     }
+// // // }
 
-// // // 2. Check whether a person is eligible for vote or not
-// // let age = 17;
+// // // // 2. Check whether a person is eligible for vote or not
+// // // let age = 17;
 
-// // if (age >= 18) {
-// //     console.log("Eligible for voting");
-// // } else {
-// //     console.log("Not eligible for voting");
-// // }
+// // // if (age >= 18) {
+// // //     console.log("Eligible for voting");
+// // // } else {
+// // //     console.log("Not eligible for voting");
+// // // }
 
-// // // 3. Check whether a person is a senior citizen or not
-// // age = 65;
+// // // // 3. Check whether a person is a senior citizen or not
+// // // age = 65;
 
-// // if (age >= 60) {
-// //     console.log("Senior Citizen");
-// // } else {
-// //     console.log("Not a Senior Citizen");
-// // }
+// // // if (age >= 60) {
+// // //     console.log("Senior Citizen");
+// // // } else {
+// // //     console.log("Not a Senior Citizen");
+// // // }
 
-// // // 4. Check whether a number is positive or negative
-// // let num = -5;
+// // // // 4. Check whether a number is positive or negative
+// // // let num = -5;
 
-// // if (num > 0) {
-// //     console.log("Positive number");
-// // } else {
-// //     if (num < 0) {
-// //         console.log("Negative number");
-// //     } else {
-// //         console.log("Number is zero");
-// //     }
-// // }
-
-
-// // // 5. Check whether a number is even or odd
-// // let number = 7;
-
-// // if (number % 2 === 0) {
-// //     console.log("Even number");
-// // } else {
-// //     console.log("Odd number");
-// // }
+// // // if (num > 0) {
+// // //     console.log("Positive number");
+// // // } else {
+// // //     if (num < 0) {
+// // //         console.log("Negative number");
+// // //     } else {
+// // //         console.log("Number is zero");
+// // //     }
+// // // }
 
 
+// // // // 5. Check whether a number is even or odd
+// // // let number = 7;
 
-// // let length = 5;
-// // let breadth = 10;
-
-// // if (length === breadth) {
-// //   console.log("Square");
-// // } else {
-// //   console.log("Rectangle");
-// // }
-
-
-// // let num1 = 10;
-// // let num2 = 20;
-// // let num3 = 30;
-
-// // if (num1 > num2 && num1 > num3) { // num1 is the largest
-// //     if (num2 > num3) {
-// //         console.log("num2 is 2nd largest");
-// //     } else {
-// //         console.log("num3 is 2nd largest");
-// //     }
-// // } else if (num2 > num1 && num2 > num3) { // num2 is the largest
-// //     if (num1 > num3) {
-// //         console.log("num1 is 2nd largest");
-// //     } else {
-// //         console.log("num3 is 2nd largest");
-// //     }
-// // } else { // num3 is the largest
-// //     if (num1 > num2) {
-// //         console.log("num1 is 2nd largest");
-// //     } else {
-// //         console.log("num2 is 2nd largest");
-// //     }
-// // }
+// // // if (number % 2 === 0) {
+// // //     console.log("Even number");
+// // // } else {
+// // //     console.log("Odd number");
+// // // }
 
 
 
+// // // let length = 5;
+// // // let breadth = 10;
 
-// // Write a JavaScript conditional statement to sort three numbers. Display an alert box to 
-// // show the results.    
-// // Sample numbers : 0, -1, 4    
-// // Output : 4, 0, -1
-
-// // let a = 0, b = -1, c = 4;
-
-// // if (a >= b && a >= c) {
-// //     if (b >= c) {
-// //         alert(a + ", " + b + ", " + c);
-// //     } else {
-// //         alert(a + ", " + c + ", " + b);
-// //     }
-// // } else if (b >= a && b >= c) {
-// //     if (a >= c) {
-// //         alert(b + ", " + a + ", " + c);
-// //     } else {
-// //         alert(b + ", " + c + ", " + a);
-// //     }
-// // } else {
-// //     if (a >= b) {
-// //         alert(c + ", " + a + ", " + b);
-// //     } else {
-// //         alert(c + ", " + b + ", " + a);
-// //     }
-// // }
+// // // if (length === breadth) {
+// // //   console.log("Square");
+// // // } else {
+// // //   console.log("Rectangle");
+// // // }
 
 
-// // let a = 3;
-// // let b = -7;
-// // let c = 2;
+// // // let num1 = 10;
+// // // let num2 = 20;
+// // // let num3 = 30;
 
-// // if (a * b * c > 0) {
-// //     console.log("The sign is +");
-// // } else if (a * b * c < 0) {
-// //     console.log("The sign is -");
-// // } else {
-// //     console.log("The sign is 0");
-// // }
+// // // if (num1 > num2 && num1 > num3) { // num1 is the largest
+// // //     if (num2 > num3) {
+// // //         console.log("num2 is 2nd largest");
+// // //     } else {
+// // //         console.log("num3 is 2nd largest");
+// // //     }
+// // // } else if (num2 > num1 && num2 > num3) { // num2 is the largest
+// // //     if (num1 > num3) {
+// // //         console.log("num1 is 2nd largest");
+// // //     } else {
+// // //         console.log("num3 is 2nd largest");
+// // //     }
+// // // } else { // num3 is the largest
+// // //     if (num1 > num2) {
+// // //         console.log("num1 is 2nd largest");
+// // //     } else {
+// // //         console.log("num2 is 2nd largest");
+// // //     }
+// // // }
 
 
 
-// // let year = 2025;
 
-// // if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-// //     console.log(year + " is a leap year");
-// // } else {
-// //     console.log(year + " is not a leap year");
-// // }
+// // // Write a JavaScript conditional statement to sort three numbers. Display an alert box to 
+// // // show the results.    
+// // // Sample numbers : 0, -1, 4    
+// // // Output : 4, 0, -1
+
+// // // let a = 0, b = -1, c = 4;
+
+// // // if (a >= b && a >= c) {
+// // //     if (b >= c) {
+// // //         alert(a + ", " + b + ", " + c);
+// // //     } else {
+// // //         alert(a + ", " + c + ", " + b);
+// // //     }
+// // // } else if (b >= a && b >= c) {
+// // //     if (a >= c) {
+// // //         alert(b + ", " + a + ", " + c);
+// // //     } else {
+// // //         alert(b + ", " + c + ", " + a);
+// // //     }
+// // // } else {
+// // //     if (a >= b) {
+// // //         alert(c + ", " + a + ", " + b);
+// // //     } else {
+// // //         alert(c + ", " + b + ", " + a);
+// // //     }
+// // // }
 
 
-// // 90 and above : A
-// // 75 to 89 : B
-// //50 to 74 : C
-// //below 50 : f
-// // let marks = 78;
+// // // let a = 3;
+// // // let b = -7;
+// // // let c = 2;
 
-// // if (marks >= 90 && marks <= 100) {
-// //     console.log("Grade: A");
-// // } else if (marks >= 75 && marks<= 89) {
-// //     console.log("Grade: B");
-// // } else if (marks >= 50 && marks<= 74) {
-// //     console.log("Grade: C");
-// // }else if(marks<= 50 && marks >= 0) {
-// //     console.log('Grade F')
-// // }
-
-// // else {
-// //     console.log("Invalid");
-// // }
-
-
-// // let char = 'A';
-
-// // if (
-// //     char === 'a' ||
-// //     char === 'e' ||
-// //     char === 'i' ||
-// //     char === 'o' ||
-// //     char === 'u' ||
-// //     char === 'A' ||
-// //     char === 'E' ||
-// //     char === 'I' ||
-// //     char === 'O' ||
-// //     char === 'U'
-// // ) {
-// //     console.log(char + " is a vowel");
-// // } else {
-// //     console.log(char + " is not a vowel");
-// // }
+// // // if (a * b * c > 0) {
+// // //     console.log("The sign is +");
+// // // } else if (a * b * c < 0) {
+// // //     console.log("The sign is -");
+// // // } else {
+// // //     console.log("The sign is 0");
+// // // }
 
 
 
-// // let num = 4;
-// //  let res = num % 2 === 0 ? "Even" : "Odd"
-// //  console.log(res);
+// // // let year = 2025;
+
+// // // if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+// // //     console.log(year + " is a leap year");
+// // // } else {
+// // //     console.log(year + " is not a leap year");
+// // // }
+
+
+// // // 90 and above : A
+// // // 75 to 89 : B
+// // //50 to 74 : C
+// // //below 50 : f
+// // // let marks = 78;
+
+// // // if (marks >= 90 && marks <= 100) {
+// // //     console.log("Grade: A");
+// // // } else if (marks >= 75 && marks<= 89) {
+// // //     console.log("Grade: B");
+// // // } else if (marks >= 50 && marks<= 74) {
+// // //     console.log("Grade: C");
+// // // }else if(marks<= 50 && marks >= 0) {
+// // //     console.log('Grade F')
+// // // }
+
+// // // else {
+// // //     console.log("Invalid");
+// // // }
+
+
+// // // let char = 'A';
+
+// // // if (
+// // //     char === 'a' ||
+// // //     char === 'e' ||
+// // //     char === 'i' ||
+// // //     char === 'o' ||
+// // //     char === 'u' ||
+// // //     char === 'A' ||
+// // //     char === 'E' ||
+// // //     char === 'I' ||
+// // //     char === 'O' ||
+// // //     char === 'U'
+// // // ) {
+// // //     console.log(char + " is a vowel");
+// // // } else {
+// // //     console.log(char + " is not a vowel");
+// // // }
 
 
 
-// // let days = "8"
-// // switch (days) {
-// //     case "1":
-// //         console.log('Sunday');
+// // // let num = 4;
+// // //  let res = num % 2 === 0 ? "Even" : "Odd"
+// // //  console.log(res);
+
+
+
+// // // let days = "8"
+// // // switch (days) {
+// // //     case "1":
+// // //         console.log('Sunday');
         
-// //         break;
-// //         case "2":
-// //         console.log('Monday');
-// //         break;
-// //     case "3":
-// //         console.log('Tuesday');
-// //         break;
-// //     case "4":
-// //         console.log('Wednesday');
-// //         break;
-// //     case "5":
-// //         console.log('Thursday');
-// //         break;
-// //     case "6":
-// //         console.log('Friday');
-// //         break;
-// //     case "7":
-// //         console.log('Saturday');
-// //         break;
+// // //         break;
+// // //         case "2":
+// // //         console.log('Monday');
+// // //         break;
+// // //     case "3":
+// // //         console.log('Tuesday');
+// // //         break;
+// // //     case "4":
+// // //         console.log('Wednesday');
+// // //         break;
+// // //     case "5":
+// // //         console.log('Thursday');
+// // //         break;
+// // //     case "6":
+// // //         console.log('Friday');
+// // //         break;
+// // //     case "7":
+// // //         console.log('Saturday');
+// // //         break;
         
+// // //     default:
+// // //         case "8":
+// // //         console.log('Invalid day');
+// // //         break;
+// // // }
+
+
+// // let num1 = 5;
+// // let num2 = 10;
+// // let operator = "-";
+
+// // switch (operator) {
+// //     case "+":
+// //         console.log("Addition: " + (num1 + num2));
+// //         break;
+// //     case "-":
+// //         console.log("Subtraction: " + (num1 - num2));
+// //         break;
+// //     case "*":
+// //         console.log("Multiplication: " + (num1 * num2));
+// //         break;
+// //     case "/":
+// //         if (num2 !== 0) {
+// //             console.log("Division: " + (num1 / num2));
+// //         } else {
+// //             console.log("Cannot divide by zero");
+// //         }
+// //         break;
+// //     case "%":
+// //         console.log("Modulus: " + (num1 % num2));
+// //         break;
 // //     default:
-// //         case "8":
-// //         console.log('Invalid day');
-// //         break;
+// //         console.log("Invalid operator");
 // // }
 
 
-// let num1 = 5;
-// let num2 = 10;
-// let operator = "-";
 
-// switch (operator) {
-//     case "+":
-//         console.log("Addition: " + (num1 + num2));
-//         break;
-//     case "-":
-//         console.log("Subtraction: " + (num1 - num2));
-//         break;
-//     case "*":
-//         console.log("Multiplication: " + (num1 * num2));
-//         break;
-//     case "/":
-//         if (num2 !== 0) {
-//             console.log("Division: " + (num1 / num2));
-//         } else {
-//             console.log("Cannot divide by zero");
-//         }
-//         break;
-//     case "%":
-//         console.log("Modulus: " + (num1 % num2));
-//         break;
-//     default:
-//         console.log("Invalid operator");
+
+
+// // LOOPS: a block of code will execute until certain condition met.
+// // why we need loop?
+// // Hello world 10th times
+// // 1 - 10
+// // 100 - 50
+// // Basic js:-> While loop, do-while,for
+// // WHILE LOOP: conditional based
+// // syntax:
+// // initization;
+// // while(condition){ // if true
+// // code to be executed;
+// // increment/decrement
+// // }
+
+// // 1 - 10
+// // let num = 1;
+// // // while(num <= 10) {
+// // while (num < 11) { // 10 < 11 , 2<11,3<11 , 11 < 11(false)
+// //     console.log(num); // 1,2,3
+// //     // num = num + 1; // 1 + 1 =2,2+1=3,3+1=4 , 10+1=11
+// //     num++;
+// // }
+// // console.log("Loop End")
+
+// // let value = 1;
+// // while (value <= 10) {
+// //     document.writeln("Hello World...");
+// //     document.writeln("<br>")
+// //     value++;
+// // }
+
+// // Print the series: 10,20,30,40,...,100
+// // let num = 10;
+// // while (num <= 100) {
+// //     console.log(num)
+// //     // num = num + 10;
+// //     num += 10;
+// // }
+// // Print the series: 105,98,91,....,7
+// // let num = 105;
+// // while (num >= 7) {
+// //     console.log(num);
+// //     num -= 7;
+// // }
+
+// // print even numbers 4 - 20
+// // let even_num = 4;
+// // while (even_num <= 20) { // 4 - 20
+// //     if (even_num % 2 == 0) {
+// //         console.log("Even Number: " + even_num);
+// //     } else {
+// //         console.log("Odd Number: " + even_num)
+// //     }
+// //     even_num++;
+// // }
+// // calculate the sum of even numbers 10 - 50
+// // let num = 10;
+// // let sum = 0;
+// // while (num <= 50) { // 10 - 50
+// //     if (num % 2 == 0) { // even numbers - 10,12,14,...50
+// //         sum += num;
+// //         // sum = sum + num;
+// //     }
+// //     num++;
+// // }
+// // console.log("The sum of even numbers: " + sum);
+
+
+// let guess = prompt("Guess a number..?")
+// let secretNumber = 10;
+// // print you guessed it right
+// // try again
+
+// while (guess != secretNumber) { // 10 == 10
+//     guess = prompt("Try again!")
 // }
-
-
-
+// alert("Correct !")
 
 
 // LOOPS: a block of code will execute until certain condition met.
@@ -323,15 +402,16 @@
 // console.log("The sum of even numbers: " + sum);
 
 
-let guess = prompt("Guess a number..?")
-let secretNumber = 10;
-// print you guessed it right
-// try again
+// let guess = prompt("Guess a number..?")
+// let secretNumber = 10;
+// // print you guessed it right
+// // try again
 
-while (guess != secretNumber) { // 10 == 10
-    guess = prompt("Try again!")
-}
-alert("Correct !")
+// while (guess != secretNumber) { // 10 == 10
+//     guess = prompt("Try again!")
+// }
+// alert("Correct !")
+
 
 
 
